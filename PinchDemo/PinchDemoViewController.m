0e5e721f6e463934fd76aa368178923e75073ea5
Adam Loving <adamloving@gmail.com>
2014-01-06 20:52:26 -0800

    Not working the way I want it to

--- PinchDemo/PinchDemoViewController.m
+++ PinchDemo/PinchDemoViewController.m
@@ -9,21 +9,25 @@
 #import "PinchDemoViewController.h"
 
 @interface PinchDemoViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation PinchDemoViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+CGFloat _lastScale = 1.0;
+- (IBAction)pinch:(UIPinchGestureRecognizer *)sender {
+    if ([sender state] == UIGestureRecognizerStateBegan) {
+        _lastScale = 1.0;
+    }
+    
+    CGFloat scale = 1.0 - (_lastScale - [sender scale]);
+    _lastScale = [sender scale];
+    CGAffineTransform currentTransform = self.imageView.transform;
+    CGAffineTransform newTransform = CGAffineTransformScale(currentTransform, scale, scale);
+    [self.imageView setTransform:newTransform];
+    NSLog(@"center: %@", NSStringFromCGPoint(self.imageView.center));
 }
 
 @end
